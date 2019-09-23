4ff5a8a58bbc5200af413371ae99a879def99217
MichaelDoherty00 <55670227+MichaelDoherty00@users.noreply.github.com>
2019-09-23 13:49:54 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp
+ *  
+ * Version information
+ * Author: Michael Doherty
+ * Date: 23/09/2019
+ * 
+ * Copyright notice
+ */
+
+#include <iostream> //iostream  is a header file that specifies functions for input and output
+
+int main() {
+  
+  std::cout << "Hello World!" << std::endl; // << = output operator
+  std::cout << "How are you today?" << " cool" << std::endl; // endl = end line
+
+  std::cout<< "C++" << std::endl;
+  
+  return 0;
+
+}
